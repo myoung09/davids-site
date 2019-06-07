5ce53f498f869230aaddcfd877772b36d4e1f277
Michael <mike.young09.my@gmail.com>
2019-06-07 13:03:05 -0500

    Updates

---
+++ precache-manifest.02b216eeb3999b48d3f7e3e4f6212b73.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "893a8d398dea985d9bfabbd564b88189",
+    "url": "/davids-site/index.html"
+  },
+  {
+    "revision": "9bbfc3774d95d55882d2",
+    "url": "/davids-site/static/css/main.c1c0d96b.chunk.css"
+  },
+  {
+    "revision": "2a030385037c72458674",
+    "url": "/davids-site/static/js/2.07af2cd4.chunk.js"
+  },
+  {
+    "revision": "9bbfc3774d95d55882d2",
+    "url": "/davids-site/static/js/main.e165a1b0.chunk.js"
+  },
+  {
+    "revision": "fd5dfecd6699825623eb",
+    "url": "/davids-site/static/js/runtime~main.4cc95c90.js"
+  },
+  {
+    "revision": "a1b1f1ddb0154a39e0ac9c61f27dbcaa",
+    "url": "/davids-site/static/media/facebook.a1b1f1dd.png"
+  },
+  {
+    "revision": "3958923ce13d950454276255f427b6d5",
+    "url": "/davids-site/static/media/landing-background.3958923c.jpg"
+  },
+  {
+    "revision": "519abdaa06c48aba290554f17eb8def6",
+    "url": "/davids-site/static/media/logo2.519abdaa.jpg"
+  },
+  {
+    "revision": "081512378a4125fc9165a8c12fd8d0fe",
+    "url": "/davids-site/static/media/logo3.08151237.jpg"
+  }
+]);
